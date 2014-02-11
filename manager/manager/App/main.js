331a6f39e4ed4ea2c97de38bdf9ff5745690b40a
MonteRio <plo.91ua@gmail.com>
2014-02-11 21:29:04 +0200

    Add durandal

---
+++ manager/manager/App/main.js
@@ -0,0 +1,36 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../Scripts/text',
+        'durandal': '../Scripts/durandal',
+        'plugins': '../Scripts/durandal/plugins',
+        'transitions': '../Scripts/durandal/transitions'
+    }
+});
+
+define('jquery', function () {
+    return jQuery;
+});
+
+define('knockout', function () {
+    return ko;
+});
+
+define(['durandal/system', 'durandal/app'],
+    function (system, app) {
+        system.debug(true);
+
+        app.title = "easygenerator";
+
+        app.configurePlugins({
+            router: true,
+            dialog: true,
+            http: true,
+            widget: true
+        });
+
+        app.start().then(function () {
+            app.setRoot('viewmodels/shell');
+        });
+
+    }
+);
